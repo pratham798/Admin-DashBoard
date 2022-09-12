c3fe10d6da7407736a65f1fb32f9c9a3f97cbb63
pratham798 <prathamjamnal7982@gmail.com>
2022-09-12 17:35:38 +0530

    Create Data Tables,Calendar and Text-Editor

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { GridComponent,ColumnsDirective,ColumnDirective,Resize,Sort,ContextMenu,Filter,Page,ExcelExport,PdfExport,Edit,Inject } from '@syncfusion/ej2-react-grids';
+import { ordersData,contextMenuItems,ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>
-      
+    <div className='bg-white m-2 md:m-10 rounded-3xl p-2 md:p-10'>
+      <Header category="Page" title="Orders"/>
+      <br></br>
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging allowSorting>
+        <ColumnsDirective>
+         {ordersGrid.map((item,index)=>(
+          <ColumnDirective key={index} {...item}/>
+         ))}
+        </ColumnsDirective>
+        <Inject services={[Resize,Sort,ContextMenu,Filter,Page,ExcelExport,Edit,PdfExport]}/>
+      </GridComponent>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({category,title}) => {
   return (
     <div>
-      
+      <p className='text-gray-400 '>
+        {category}
+      </p>
+      <p className='text-3xl font-extrabold tracking-tight text-slate-900'>
+        {title}
+      </p>
     </div>
-  )
+  );
 }
 
 export default Header
